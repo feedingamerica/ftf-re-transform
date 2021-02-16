0b20c6db780064abf74238ef615bf0ddc421a79c
Foodshops <32028050+Foodshops@users.noreply.github.com>
2021-02-16 10:24:49 -0500

    sample added, beginner stuff
    yeah

---
+++ transformlayer/transformapi/library.py
@@ -0,0 +1,37 @@
+sample_dict = {
+    "Scope": {
+        "startDate":"01/01/2019",
+        "endDate":"12/31/2019",
+        "scope_field":"fb_id",
+        "scope_field_value":21,
+        "control_type_field":"dummy_is_grocery_service",
+        "control_type_value":1
+    },
+    "ReportInfo": {
+        [
+            {
+                "reportId":1,
+                "reportDictId":1,
+                "dateDefId":1,
+                "name":"name"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dateDefId":1,
+                "name":"name"
+            },
+            {
+                "reportId":1,
+                "reportDictId":3,
+                "dateDefId":1,
+                "name":"name"
+            }
+        ]
+    }
+}
+
+def parse_dict(dict):
+
+
+    return dict
