3912c94cfd9d012bf8088a810239a06564c7b57f
cspanos <spanoschristina@gmail.com>
2021-02-18 10:13:32 -0500

    fix test data endpoint to work

--- transformlayer/transformapi/views.py
+++ transformlayer/transformapi/views.py
@@ -46,11 +46,50 @@ def test_endpoint_2(request):
     params = parse_request(sample_dict)
     num_services = calc.get_services_total(params)
     num_families = calc.get_undup_hh_total(params)
-    print("Number of unduplicated services " + str(num_services))
-    print("Number of unduplicated families " + str(num_families))
 
-    return HttpResponse('We are FTF.')
+    response = "Number of unduplicated services " + str(num_services)
+    response += "\n"
+    response += "Number of unduplicated families " + str(num_families)
+    
+    print(response)
+    return HttpResponse(response)
 
 def test_data_service(request, id):
+    sample_dict = {
+        "Scope": {
+            "startDate":"01/01/2019",
+            "endDate":"12/31/2019",
+            "scope_field":"fb_id",
+            "scope_field_value":21,
+            "control_type_field":"dummy_is_grocery_service",
+            "control_type_value":1
+        },
+        "ReportInfo": [
+            {
+                "reportId":1,
+                "reportDictId":1,
+                "dataDefId":1,
+                "name":"name_one",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":2,
+                "reportDictId":2,
+                "dataDefId":2,
+                "name":"name_two",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":3,
+                "reportDictId":3,
+                "dataDefId":3,
+                "name":"name_three",
+                "dataDefType":"type1"
+            }
+        ]
+    }
+    params = parse_request(sample_dict)
 
-    return HttpResponse(str(id) + "\t" + Data_Service.get_data_for_definition(id))
+    data = Data_Service.get_data_for_definition(id, params)
+    print(data)
+    return HttpResponse(str(id) + "\t" + str(data))
