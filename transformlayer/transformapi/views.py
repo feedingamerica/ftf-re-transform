4f56b1c061090e82825f803d10dc040c028bd674
cspanos <spanoschristina@gmail.com>
2021-02-22 16:07:19 -0500

    update endpoint

--- transformlayer/transformapi/views.py
+++ transformlayer/transformapi/views.py
@@ -97,7 +97,7 @@ def test_data_service(request, id):
     print(data)
     return HttpResponse(str(id) + "\t" + str(data))
 
-def get_report(request):
+def get_report_big_numbers(request):
     input_dict = {
         "Scope": {
             "startDate":"01/01/2019",
@@ -133,6 +133,133 @@ def get_report(request):
                 "reportId":4,
                 "reportDictId":4,
                 "dataDefId":4,
+                "name":"name_four",
+                "dataDefType":"type1"
+            }
+        ]
+    }
+
+    params = parse_request(input_dict)
+    cd = CalculationDispatcher(params)
+    cd.run_calculations()
+
+    context = { 'report_output': format_dict(cd.request) }
+    print_dict(input_dict)
+    return render(request, 'transformapi/get-report.html', context)
+
+def get_report_ohio(request):
+    input_dict = {
+        "Scope": {
+            "startDate":"01/01/2019",
+            "endDate":"12/31/2019",
+            "scope_field":"fb_id",
+            "scope_field_value":21,
+            "control_type_field":"dummy_is_grocery_service",
+            "control_type_value":1
+        },
+        "ReportInfo": [
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":5,
+                "name":"name_one",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":6,
+                "name":"name_two",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":7,
+                "name":"name_three",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":8,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":9,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":10,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":11,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":12,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":13,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":14,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":15,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":16,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":17,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":18,
+                "name":"name_four",
+                "dataDefType":"type1"
+            },
+            {
+                "reportId":1,
+                "reportDictId":2,
+                "dataDefId":19,
                 "name":"name_four",
                 "dataDefType":"type1"
             }
